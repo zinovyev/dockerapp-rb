16b787f9fd96a3f5d8235d20a83455ca8f8c167e
Ivan Zinovyev <vanyazin@gmail.com>
2017-10-23 19:23:28 +0300

    Store values to redis

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'sinatra', '~> 1.4', '>= 1.4.7'
+gem 'redis', '~> 4.0', '>= 4.0.1'
--- app/app.rb
+++ app/app.rb
@@ -1,18 +1,21 @@
 require 'sinatra'
+require 'redis'
 
 set :bind, '0.0.0.0'
 default_key = '1'
-cache = { default_key => 'one' }
+cache = Redis.new(host: 'redis', port: 6379, db: 0)
+cache.set(default_key, 'one')
 
 %i[get post].each do |m|
   send m, '/' do
     key = params.key?('key') ? params['key'] : default_key
 
     if request.post? && params['submit'] == 'save'
-      cache[key] = params['cache_value']
+      cache.set(key, params['cache_value'])
     end
 
-    cache_value = cache[key] if cache.key? key
+    value = cache.get(key)
+    cache_value = value if value
 
     erb :index, locals: { key: key, cache_value: cache_value }
   end
