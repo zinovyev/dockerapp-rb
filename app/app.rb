38689929d9f516b77cd374c33078fe8716bd5561
Ivan Zinovyev <vanyazin@gmail.com>
2017-10-23 19:01:39 +0300

    Single service app

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,19 @@
-# myapp.rb
 require 'sinatra'
 
 set :bind, '0.0.0.0'
+default_key = '1'
+cache = { default_key => 'one' }
 
-get '/' do
-  'Hello, World!'
+%i[get post].each do |m|
+  send m, '/' do
+    key = params.key?('key') ? params['key'] : default_key
+
+    if request.post? && params['submit'] == 'save'
+      cache[key] = params['cache_value']
+    end
+
+    cache_value = cache[key] if cache.key? key
+
+    erb :index, locals: { key: key, cache_value: cache_value }
+  end
 end
